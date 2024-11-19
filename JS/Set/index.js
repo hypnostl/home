198334d531c370867008142a3acd56c0038a7ea0
Thiago Lopes <hypnostl@gmail.com>
2024-11-19 17:30:12 -0300

    Add setTimeout() e setIntetval()

---
+++ JS/Set/index.js
@@ -0,0 +1,16 @@
+console.log("Iniciando programa")
+
+
+
+
+let seconds = 0
+
+const programa = setInterval(() => {
+    seconds += 3
+   console.log("Programa rodando") 
+   if(seconds > 10)
+   {clearInterval(programa)
+    {
+    console.log("Progrma finalizado")}}
+}, 3000);
+
