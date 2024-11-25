57144a33cbc47456665c3e82a34fffb0b88dd6c0
Thiago Lopes <hypnostl@gmail.com>
2024-11-25 18:11:00 -0300

    Atualiza exercicio primises

--- JS/Exercicio_promises/index.js
+++ JS/Exercicio_promises/index.js
@@ -1,24 +1,25 @@
-
-
-function imc (peso, altura){
-
-return new Promise((resolve, reject)=>{
-    peso 
-    altura 
-   if(!Number.isNaN(peso,altura))
-   {
-    resolve(console.log(`Peso é ${peso}kg e Altura é ${altura}m`))
-}else{
-reject(console.log("Não é numero"))
-}
-})
+function imc(peso, altura) {
+  return new Promise((resolve, reject) => {
+    peso;
+    altura;
+    if (!Number.isNaN(peso / altura)) {
+      console.log(peso / altura);
+      resolve(`Peso é ${peso}kg e Altura é ${altura}m. O IMC é ${peso/(altura*altura)}`);
+    } else {
+      reject("Não é numero");
+    }
+  });
 }
 
-const thiago = imc(1,10)
+const thiago = imc(88,1.79);
+console.log(thiago);
 
-thiago.then(()=>{
-    console.log("Foi resolvido")
-}).catch((err)=>{
-    console.log("Rejeitada")
-})
-
+thiago.then(() => {
+    console.log("Foi resolvido");
+  })
+  .catch((err) => {
+    console.log(`Rejeitada ${err}`);
+  })
+  .finally(() => {
+    console.log("Finalizada!");
+  });
