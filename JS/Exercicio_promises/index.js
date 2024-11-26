47eb9b9cf6ce700b8f0647236f34464cc1b88ecd
Thiago Lopes <hypnostl@gmail.com>
2024-11-26 12:20:20 -0300

    Exercicio finalizado

--- JS/Exercicio_promises/index.js
+++ JS/Exercicio_promises/index.js
@@ -3,23 +3,41 @@ function imc(peso, altura) {
     peso;
     altura;
     if (!Number.isNaN(peso / altura)) {
-      console.log(peso / altura);
-      resolve(`Peso é ${peso}kg e Altura é ${altura}m. O IMC é ${peso/(altura*altura)}`);
+      console.log(`O peso é ${peso} e a altura é ${altura}`)
+      resolve(peso / (altura*altura));
     } else {
       reject("Não é numero");
     }
   });
 }
 
-const thiago = imc(88,1.79);
-console.log(thiago);
 
-thiago.then(() => {
-    console.log("Foi resolvido");
-  })
-  .catch((err) => {
-    console.log(`Rejeitada ${err}`);
-  })
-  .finally(() => {
-    console.log("Finalizada!");
-  });
+function calculoImc(peso, altura)
+{
+  const pessoa = imc(peso,altura);
+  
+  
+  pessoa.then((resultado) => {
+    if (resultado < 18.5) {
+      console.log("Mgraza");
+  } else if (resultado >= 18.5 && resultado <= 24.9) {
+      console.log("Normal");
+  } else if (resultado >= 25 && resultado <= 29.9) {
+      console.log("Sobrepeso");
+  } else if (resultado >= 30 && resultado <= 39.9) {
+      console.log("Obesidade");
+  } else if (resultado >= 40) {
+      console.log("Obedidade grave");
+  } else {
+      return "Valor inválido";
+  }
+    })
+    .catch((err) => {
+      console.log(`Rejeitada ${err}`);
+    })
+    .finally(() => {
+      console.log("Finalizada!");
+    });
+}
+
+calculoImc("aa",1.8)
