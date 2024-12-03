efc948c8f9b694328eaefcdd840778b988910c43
Thiago Lopes <hypnostl@gmail.com>
2024-12-03 17:53:00 -0300

    Exercicio promises finalizado!

--- JS/Exercicio_promises/index.js
+++ JS/Exercicio_promises/index.js
@@ -1,43 +1,40 @@
 function imc(peso, altura) {
   return new Promise((resolve, reject) => {
-    peso;
-    altura;
-    if (!Number.isNaN(peso / altura)) {
-      console.log(`O peso é ${peso} e a altura é ${altura}`)
-      resolve(peso / (altura*altura));
+    if (typeof peso !== 'number' || typeof altura !== 'number') {
+      reject("Peso ou aultura não é numero!!!");
     } else {
-      reject("Não é numero");
+      
+      resolve(peso / (altura * altura));
     }
   });
 }
 
-
 function calculoImc(peso, altura)
 {
-  const pessoa = imc(peso,altura);
-  
-  
-  pessoa.then((resultado) => {
+  imc(peso,altura).then((resultado) => {
+    console.log(`O peso é ${peso} e a altura é ${altura}`);
     if (resultado < 18.5) {
-      console.log("Mgraza");
-  } else if (resultado >= 18.5 && resultado <= 24.9) {
-      console.log("Normal");
-  } else if (resultado >= 25 && resultado <= 29.9) {
-      console.log("Sobrepeso");
-  } else if (resultado >= 30 && resultado <= 39.9) {
-      console.log("Obesidade");
-  } else if (resultado >= 40) {
-      console.log("Obedidade grave");
-  } else {
+      console.log(`O IMC é ${resultado}. Você está com magreza.`);
+    } else if (resultado >= 18.5 && resultado <= 24.9) {
+      console.log(`O IMC é ${resultado}. Você está com peso normal.`);
+    } else if (resultado >= 25 && resultado <= 29.9) {
+      console.log(`O IMC é ${resultado}. Você está com sobrepeso.`);
+    } else if (resultado >= 30 && resultado <= 39.9) {
+      console.log(`O IMC é ${resultado}. Você está com obesidade.`);
+    } else if (resultado >= 40) {
+      console.log(`O IMC é ${resultado}. Você está com obesidade grave.`);
+    } else {
       return "Valor inválido";
-  }
-    })
-    .catch((err) => {
-      console.log(`Rejeitada ${err}`);
-    })
-    .finally(() => {
-      console.log("Finalizada!");
-    });
+    }
+  })
+  .catch((err) => {
+    console.log(`Rejeitada ${err}`);
+  })
+  .finally(() => {
+    console.log("Finalizada!");
+  });
 }
 
-calculoImc("aa",1.8)
+calculoImc(88, 1.8);
+
+calculoImc(120,1,8)
