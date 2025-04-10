4b9f4f0c3ca27aa8fe94fd223aaabc1e92920a90
Thiago Lopes <hypnostl@gmail.com>
2025-04-10 18:08:42 -0300

    Atualizaçao

--- Node_Web/04-html-dinamico-ejs/src/index.js
+++ Node_Web/04-html-dinamico-ejs/src/index.js
@@ -1,14 +1,41 @@
-const express = require ('express')
+const express = require('express')
 const path = require('node:path')
 
 const app = express()
 
-app.set('view engine')
+const storeUsers = []
+//configuração do EJS
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res)=>{
-    res.send("Hello world")
+//configuração do body
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  const title = "Homepage"
+  const message = "Mensagem dinamica de teste em EJS"
+
+  res.render('index',{title, message})
 })
 
+app.get('/formulario', (req,res)=>{
+  res.render('form')
+})
 
+app.post('/register', (req, res)=>{
+const username = req.body.username
+const passworld = req.body.passworld
+
+
+storeUsers.push(username, passworld)
+res.redirect('/usuarios')
+})
+
+app.get('/usuarios',(req, res)=>{
+  res.render('users', {users:storeUsers})
+})
 const PORT = 3000
-app.listen(PORT, ()=>console.log(`Servidor iniciado!`))
+app.listen(PORT, () => {
+  console.log('Servidor iniciado!')
+})
+
